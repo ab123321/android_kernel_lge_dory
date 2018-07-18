ef6c184625624365bd9e644022aa3e3e21db9aee
Artem Labazov <123321artyom@gmail.com>
2018-07-18 15:28:12 +0300

    touch: synaptics: use original dory driver
    The dory driver has diverged significantly from Qualcomm's.
    Revert to using the original dory driver.
    
    Change-Id: I53274fbce40c250d3b2b3b0826736ff26ab5824f
    Signed-off-by: Simon Wilson <simonwilson@google.com>
    Signed-off-by: Artem Labazov <123321artyom@gmail.com>

--- drivers/input/touchscreen/synaptics_fw_update.c
+++ drivers/input/touchscreen/synaptics_fw_update.c
@@ -26,7 +26,6 @@
 #include <linux/input.h>
 #include <linux/firmware.h>
 #include <linux/string.h>
-#include <linux/ctype.h>
 #include <linux/input/synaptics_dsx.h>
 #include "synaptics_i2c_rmi4.h"
 
@@ -258,7 +257,6 @@ struct synaptics_rmi4_fwu_handle {
 	bool interrupt_flag;
 	bool polling_mode;
 	char product_id[SYNAPTICS_RMI4_PRODUCT_ID_SIZE + 1];
-	unsigned int full_update_size;
 	unsigned int image_size;
 	unsigned int data_pos;
 	unsigned char intr_mask;
@@ -298,22 +296,6 @@ static struct synaptics_rmi4_fwu_handle *fwu;
 
 DECLARE_COMPLETION(fwu_remove_complete);
 
-/* Check offset + size <= bound.  1 if in bounds, 0 otherwise. */
-static bool in_bounds(unsigned long offset,
-		      unsigned long size,
-		      unsigned long bound)
-{
-	if (offset > bound || size > bound) {
-		pr_err("%s: %lu or %lu > %lu\n", __func__, offset, size, bound);
-		return 0;
-	}
-	if (offset > (bound - size)) {
-		pr_err("%s: %lu > %lu - %lu\n", __func__, offset, size, bound);
-		return 0;
-	}
-	return 1;
-}
-
 static unsigned int extract_uint(const unsigned char *ptr)
 {
 	return (unsigned int)ptr[0] +
@@ -350,17 +332,11 @@ static void synaptics_rmi4_update_debug_info(void)
 		pkg_id[3] << 8 | pkg_id[2], build_id);
 }
 
-static int parse_header(void)
+static void parse_header(void)
 {
 	struct image_content *img = &fwu->image_content;
 	struct image_header_data *data =
 		(struct image_header_data *)fwu->data_buffer;
-	if (fwu->full_update_size < sizeof(*data)) {
-		dev_err(&fwu->rmi4_data->i2c_client->dev,
-			"Provided update too small");
-		return -EINVAL;
-	}
-
 	img->checksum = extract_uint(data->file_checksum);
 	img->bootloader_version = data->bootloader_version;
 	img->image_size = extract_uint(data->firmware_size);
@@ -373,11 +349,12 @@ static int parse_header(void)
 	memcpy(img->product_info, data->product_info,
 		sizeof(data->product_info));
 
-	if (fwu->rmi4_data->check_build)
-		img->is_contain_build_info =
-			(data->options_firmware_id == (1 << OPTION_BUILD_INFO));
-	else
-		img->is_contain_build_info = 0;
+#ifdef CHECK_BUILD_INFO
+	img->is_contain_build_info =
+		(data->options_firmware_id == (1 << OPTION_BUILD_INFO));
+#else
+	img->is_contain_build_info = 0;
+#endif
 
 	if (img->is_contain_build_info) {
 		img->package_id = (data->pkg_id_msb << 8) |
@@ -400,29 +377,12 @@ static int parse_header(void)
 		img->config_size);
 
 	/* get UI firmware offset */
-	if (img->image_size) {
-		if (!in_bounds(FW_IMAGE_OFFSET, img->image_size,
-			       fwu->full_update_size)) {
-			dev_err(&fwu->rmi4_data->i2c_client->dev,
-				"%s: image size out of bounds\n",
-				__func__);
-			return -EINVAL;
-		}
+	if (img->image_size)
 		img->firmware_data = fwu->data_buffer + FW_IMAGE_OFFSET;
-	}
 	/* get config offset*/
-	if (img->config_size) {
-		// FW_IMAGE_OFFSET + image_size was ok as above
-		if (!in_bounds(FW_IMAGE_OFFSET + img->image_size,
-			       img->config_size, fwu->full_update_size)) {
-			dev_err(&fwu->rmi4_data->i2c_client->dev,
-				"%s: config size out of bounds\n",
-				__func__);
-			return -EINVAL;
-		}
+	if (img->config_size)
 		img->config_data = fwu->data_buffer + FW_IMAGE_OFFSET +
 				img->image_size;
-	}
 	/* get lockdown offset*/
 	switch (img->bootloader_version) {
 	case 3:
@@ -441,14 +401,6 @@ static int parse_header(void)
 		img->lockdown_data = NULL;
 	}
 
-	if (img->lockdown_block_count * fwu->block_size > FW_IMAGE_OFFSET) {
-		dev_err(&fwu->rmi4_data->i2c_client->dev,
-			"%s: lockdown size too big\n",
-			__func__);
-		return -EINVAL;
-	}
-	if (fwu->full_update_size < FW_IMAGE_OFFSET)
-		return -EINVAL;
 	img->lockdown_data = fwu->data_buffer +
 				FW_IMAGE_OFFSET -
 				img->lockdown_block_count * fwu->block_size;
@@ -457,7 +409,7 @@ static int parse_header(void)
 	fwu->lockdown_data = img->lockdown_data;
 	fwu->config_data = img->config_data;
 	fwu->firmware_data = img->firmware_data;
-	return 0;
+	return;
 }
 
 static int fwu_read_f01_device_status(struct f01_device_status *status)
@@ -883,15 +835,15 @@ static enum flash_area fwu_go_nogo(void)
 			"Device firmware id %d, .img firmware id %d\n",
 			deviceFirmwareID,
 			(unsigned int)imageFirmwareID);
-
-	/*
-	 * New firmware's FirmwareID isn't always increased
-	 * from the current firmware's, so just mark firmware update flag
-	 * if FirmwareIDs are different.
-	 * Firmware update will be decided by checking "config_id".
-	 */
-	if (imageFirmwareID != deviceFirmwareID) {
-		config_flag = 1;
+	if (imageFirmwareID > deviceFirmwareID) {
+		flash_area = UI_FIRMWARE;
+		goto exit;
+	} else if (imageFirmwareID < deviceFirmwareID) {
+		flash_area = NONE;
+		dev_info(&i2c_client->dev,
+			"%s: Img fw is older than device fw. Skip fw update.\n",
+			__func__);
+		goto exit;
 	}
 
 check_config_id:
@@ -908,10 +860,6 @@ check_config_id:
 		flash_area = NONE;
 		goto exit;
 	}
-	config_id[0] = toupper(config_id[0]);
-	config_id[1] = toupper(config_id[1]);
-	config_id[2] = toupper(config_id[2]);
-	config_id[3] = toupper(config_id[3]);
 	deviceConfigID =  extract_uint_be(config_id);
 
 	dev_info(&i2c_client->dev,
@@ -925,14 +873,13 @@ check_config_id:
 			fwu->config_data[1],
 			fwu->config_data[2],
 			fwu->config_data[3]);
+	imageConfigID =  extract_uint_be(fwu->config_data);
 
 	/* copy to keep debug information */
-	fwu->img_config_data[0] = toupper(fwu->config_data[0]);
-	fwu->img_config_data[1] = toupper(fwu->config_data[1]);
-	fwu->img_config_data[2] = toupper(fwu->config_data[2]);
-	fwu->img_config_data[3] = toupper(fwu->config_data[3]);
-
-	imageConfigID =  extract_uint_be(fwu->img_config_data);
+	fwu->img_config_data[0] = fwu->config_data[0];
+	fwu->img_config_data[1] = fwu->config_data[1];
+	fwu->img_config_data[2] = fwu->config_data[2];
+	fwu->img_config_data[3] = fwu->config_data[3];
 
 	dev_dbg(&i2c_client->dev,
 		"%s: Device config ID %d, .img config ID %d\n",
@@ -1354,8 +1301,8 @@ static int fwu_start_write_config(void)
 				"%s: write config from config file\n",
 				__func__);
 		fwu->config_data = fwu->data_buffer;
-	} else if (parse_header()) {
-		return -EINVAL;
+	} else {
+		parse_header();
 	}
 
 	pr_notice("%s: Start of write config process\n", __func__);
@@ -1422,8 +1369,7 @@ exit:
 
 static int fwu_start_write_lockdown(void)
 {
-	if (parse_header())
-		return -EINVAL;
+	parse_header();
 	return fwu_do_write_lockdown(true);
 }
 
@@ -1639,13 +1585,9 @@ static int fwu_start_reflash(void)
 				__func__, fw_entry->size);
 
 		fwu->data_buffer = fw_entry->data;
-		fwu->full_update_size = fw_entry->size;
 	}
 
-	if (parse_header()) {
-		retval = -EINVAL;
-		goto exit;
-	}
+	parse_header();
 	flash_area = fwu_go_nogo();
 
 	if (fwu->rmi4_data->sensor_sleep) {
@@ -2038,7 +1980,6 @@ static ssize_t fwu_sysfs_image_size_store(struct device *dev,
 	if (retval)
 		return retval;
 
-	fwu->full_update_size = size;
 	fwu->image_size = size;
 	fwu->data_pos = 0;
 
@@ -2103,11 +2044,6 @@ static ssize_t fwu_sysfs_config_id_show(struct device *dev,
 				fwu->f34_fd.ctrl_base_addr,
 				config_id,
 				sizeof(config_id));
-
-	config_id[0] = toupper(config_id[0]);
-	config_id[1] = toupper(config_id[1]);
-	config_id[2] = toupper(config_id[2]);
-	config_id[3] = toupper(config_id[3]);
 
 	return snprintf(buf, PAGE_SIZE, "DEVICE: %c%c%c%c IMG: %c%c%c%c\n",
 		config_id[0], config_id[1], config_id[2], config_id[3],
